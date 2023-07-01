fed17aa291be7fe0f5f90c9940e0ae414a2d830f
tosangeetha <tosangeetha@github.com>
2023-07-01 12:02:51 +0800

    Updated subnet

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # Create EC2 instances
 resource "aws_instance" "sangeethawebserver1" {
-  ami           = "ami-0c91f4476780c2eaf "  # Replace with the desired Amazon Linux AMI ID
+  ami           = "ami-0c91f4476780c2eaf"  # Replace with the desired Amazon Linux AMI ID
   instance_type = "t2.micro"
   key_name      = "SangeethaKeyPair"  # Replace with the name of your keypair
 
   associate_public_ip_address = true
-  subnet_id                   = "subnet-bea677f6"  # Replace with the ID of your existing public subnet
+  subnet_id                   = "subnet-29ed7170"  # Replace with the ID of your existing public subnet
   vpc_security_group_ids      = ["sg-0dd65e48722662e22"]  # Replace with the ID of your existing security group
 
   tags = {
@@ -19,7 +19,7 @@ resource "aws_instance" "sangeethawebserver2" {
   key_name      = "SangeethaKeyPair"  # Replace with the name of your keypair
 
   associate_public_ip_address = true
-  subnet_id                   = "subnet-bea677f6"  # Replace with the ID of your existing public subnet
+  subnet_id                   = "subnet-29ed7170"  # Replace with the ID of your existing public subnet
   vpc_security_group_ids      = ["sg-0dd65e48722662e22"]  # Replace with the ID of your existing security group
 
   tags = {
@@ -33,7 +33,7 @@ resource "aws_instance" "ansibleserver" {
   instance_type          = "t2.micro"
   key_name               = "SangeethaKeyPair"      # Replace with your keypair name
   associate_public_ip_address = true
-  subnet_id              = "subnet-bea677f6"  # Replace with your existing public subnet ID
+  subnet_id              = "subnet-29ed7170"  # Replace with your existing public subnet ID
   vpc_security_group_ids = ["sg-0dd65e48722662e22"]  # Replace with your existing security group ID
   
   user_data = <<-EOF
